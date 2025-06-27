cc2875933fdc60662457b96f9ff92a800fdf4be7
Felipe Aguayo <felipeaguayo212@icloud.com>
2025-06-27 17:58:41 -0600

    Implement code changes to enhance functionality and improve performance

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 const burger = document.getElementById('burger-menu');
 const nav = document.getElementById('nav-menu');
+const overlay = document.getElementById('nav-overlay');
+
 burger.addEventListener('click', () => {
-  nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+  nav.classList.add('open');
+  overlay.classList.add('active');
+});
+
+overlay.addEventListener('click', () => {
+  nav.classList.remove('open');
+  overlay.classList.remove('active');
 });
